ad41a1969c87ab1cc70dad55188abad51a7f4965
MINDEV\kin41 <kin412@naver.com>
2026-02-02 00:58:55 +0900

    다중 @Configuration 파일 컴포넌트 스캔 문제 excludeFilters 처리

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -2,8 +2,15 @@ package hello.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication // 여기에 이미 componentscan이 포함되어있다. 열어봐라
+@ComponentScan( // 이걸 추가하지 않으면 공부용으로 만들어놨던 AppConfig와 AutoAppConfig도 @Configuration 이 설정 어노테이션안에 @Component가 있기 때문에
+                // 읽게 되며 그래서 자꾸 뭐 빈이 두개니 뭐니 하는 에러가 났던 것임. 그래서 필터로 실행시에 둘다 거르도록 설정
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 public class CoreApplication {
 
     public static void main(String[] args) {
--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -2,8 +2,15 @@ package hello.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootTest
+@ComponentScan( // 이걸 추가하지 않으면 공부용으로 만들어놨던 AppConfig와 AutoAppConfig도 @Configuration 이 설정 어노테이션안에 @Component가 있기 때문에
+        // 읽게 되며 그래서 자꾸 뭐 빈이 두개니 뭐니 하는 에러가 났던 것임. 그래서 필터로 실행시에 둘다 거르도록 설정
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 class CoreApplicationTests {
 
     @Test
