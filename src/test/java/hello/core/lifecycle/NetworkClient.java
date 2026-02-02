4da8bd80eb0dba4801c4f35de9e6d990cf94785e
MINDEV\kin41 <kin412@naver.com>
2026-02-02 19:20:51 +0900

    빈 생명주기와 콜백

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,61 @@
+package hello.core.lifecycle;
+
+import ch.qos.logback.core.spi.LifeCycle;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+//implements InitializingBean, DisposableBean
+public class NetworkClient  {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url: " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect url: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message: " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disConnect() {
+        System.out.println("close url: " + url);
+    }
+
+    /* 스프링에서 제공하는 초기화, 소멸 인터페이스 상속시
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+        disConnect();
+    }*/
+
+    public void init() {
+        System.out.println("init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
+        System.out.println("close");
+        disConnect();
+    }
+
+}
